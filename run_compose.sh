f72a63ecf089d4f53dde784e5776b70079367396
Bernd Kiefer <bernd.kiefer@dfki.de>
2023-05-26 11:00:16 +0200

    clean up

--- run_compose.sh
+++ run_compose.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+cd `dirname $0`
 export AUDIO_GID="$(getent group audio | cut -d: -f3)"
 export UID
-docker compose up
+cmd="$1"
+if test -z "$cmd"; then
+    cmd="up"
+fi
+docker compose "$cmd"
